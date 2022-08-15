f4479bcc37b3ebac89ec2ac28aeecc3212c49471
hamzakh777 <hamzabitup@gmail.com>
2022-08-15 16:56:57 +0100

    test: Fund

--- test/unit/FundMe.spec.ts
+++ test/unit/FundMe.spec.ts
@@ -1,4 +1,4 @@
-import { assert } from "chai";
+import { assert, expect } from "chai";
 import { deployments, ethers, getNamedAccounts } from "hardhat";
 import { FundMe, MockV3Aggregator } from "../../typechain-types";
 
@@ -10,8 +10,8 @@ describe("FundMe", async function() {
     // deploy all contract
     deployer = (await getNamedAccounts()).deployer;
     await deployments.fixture("all");
-    // fundMe = await ethers.getContract("FundMe", deployer);
-    // mockV3Aggregator = await ethers.getContract("MockV3Aggregator", deployer);
+    fundMe = await ethers.getContract("FundMe", deployer);
+    mockV3Aggregator = await ethers.getContract("MockV3Aggregator", deployer);
   });
 
   describe("Contructor", async function() {
@@ -21,4 +21,12 @@ describe("FundMe", async function() {
     });
   });
 
+  describe("Fund", async function() {
+    it("Should fail when the eth amount sent is not enough", async function() {
+      const contractTransaction = fundMe.fund();
+      await expect(contractTransaction).to.be.revertedWith(
+        "You need to spend more ETH!"
+      );
+    });
+  });
 });
